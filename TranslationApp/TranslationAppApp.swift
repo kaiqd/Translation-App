2be240336cc24f9cad2ddf06fff20b6dc7980843
kaiqd <kaiquediniz1204@gmail.com>
2024-10-20 01:15:13 -0300

    Initial Commit

---
+++ TranslationApp/TranslationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationAppApp.swift
+//  TranslationApp
+//
+//  Created by Kaique Diniz on 20/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TranslationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranslationAppTests/TranslationAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TranslationAppTests.swift
+//  TranslationAppTests
+//
+//  Created by Kaique Diniz on 20/10/24.
+//
+
+import Testing
+@testable import TranslationApp
+
+struct TranslationAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
